ebee9e6bf3c02725eec9f75c0de2eefa4a69e090
AnujSinghR <anujsingh0r@gmail.com>
2024-02-29 17:19:38 +0530

    resume-builder-prompt

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Name from "./prompts-components/Name.jsx";
+
+function App() {
+  return (
+   <>
+   <div class="prompt-box-Name">
+          <div class="container"><div class="progress-container"></div>
+          </div>
+          <Name/>
+  </div>
+   
+   </>
+  );
+}
+
+export default App;
---
+++ src/prompts-components/Name.jsx
@@ -0,0 +1,15 @@
+import "./prompts_components.css";
+
+function Name(){
+    return(
+        <>
+        <div>
+            <p>Enter Your Name *</p>
+            <input/>
+            <button>Cancel</button>
+            <button>Submit</button>
+            </div>
+        </>
+    );
+}
+export default Name;
